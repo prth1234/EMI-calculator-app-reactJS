0859486388a466943660eb2077795b15a026f06b
prth1234 <tintin.dps@gmail.com>
2024-03-11 16:15:39 +0530

    make buttns of proper size

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -485,19 +485,22 @@ function Forms(){
           </section>
           <div className="button-group" style={{display:"flex",gap:7 , justifyContent:'center'}}>
           <button className="start" onClick={() => setStep(1)}>Start Over</button>
+          <button onClick={handleVisualize}>Visualize</button>
+
+          <button onClick={handleRepaymentSchedule}>✨Generate Amortization Table</button>
 
             {/* <button>
+
 <span class="text">EMI Scheduler</span>
 </button> */}
             {/* <button onClick={handleRepaymentSchedule}>Amortization Calculator</button> */}
-            <button onClick={handleRepaymentSchedule} class="BTNNN">
+            {/* <button onClick={handleRepaymentSchedule} class="BTNNN">
     <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" class="sparkle">
         <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
     </svg>
 
     <span class="text">Generate Amortization Table</span>
-</button>
-<button onClick={handleVisualize}>Visualize</button>
+</button> */}
 
           </div>
         </div>
